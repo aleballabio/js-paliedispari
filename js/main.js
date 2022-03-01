503752c5881f18a543a817d9e3af753e54789390
aleballabio <aleballabio05@gmail.com>
2022-03-01 18:16:56 +0100

    done with the exercises

--- js/main.js
+++ js/main.js
@@ -38,20 +38,56 @@ else {
 */
 
 const stamp = document.querySelector(".stamp");
+const stampRisultato = document.querySelector(".risultato");
 stamp.innerHTML = "Pari o Dispari Game!";
 
 let OddEvens = document.querySelector("#choice");
 let number = document.querySelector("#number");
-let numberV = parseInt(number.value);
 
-function randomNumber(max, min) {
-    Math.floor(Math.random() * (max - min + 1)) + min;
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 let btn = document.getElementById("submit");
 
 btn.addEventListener("click", function () {
+
+    let numberValue = parseInt(number.value);
+    let OddEvensValue = OddEvens.value;
+
     let randomN = randomNumber(1, 5);
-    let randomF = randomN + numberV;
-    console.log(randomF)
+    let randomF = randomN + numberValue;
+    console.log(randomF);
+
+
+
+    function NumberOddEven(number) {
+        let OddEventype = "Pari"
+
+        if (number % 2 === 0) {
+            OddEventype = "Pari"
+        }
+
+        else if (number % 2 === 1) {
+            OddEventype = "Dispari"
+        }
+
+        return OddEventype;
+    }
+
+
+
+    let numberOddEvenType = NumberOddEven(randomF);
+
+
+    if (numberOddEvenType == OddEvensValue) {
+        stampRisultato.innerHTML = ("Hai vinto perchè" + " " + randomF + " " + "è" + " " + OddEvensValue + "!");
+    }
+
+    else if (numberOddEvenType !== OddEvensValue) {
+        OddEvensValue = "Dispari"
+        stampRisultato.innerHTML = ("Hai perso perchè" + " " + randomF + " " + "è" + " " + OddEvensValue + "!");
+    }
 })
+
